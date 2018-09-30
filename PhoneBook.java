4d13acd8ea166bebb2fde1cdb7bd34d5444d1924
Zachjrivera <Zachjrivera@gmail.com>
2018-09-30 01:15:54 -0400

    All but two methods
    Missing reverseLookUp() and display() methods got worried no one was talking on slack

--- PhoneBook.java
+++ PhoneBook.java
@@ -5,11 +5,39 @@ public class PhoneBook
     /*
     here is where Zach and Zohdi will compile all of the methods
     from the other "phonebook classes" after Troy, Lamar, and Javon submit them
+*/
+
+
+      //instance variable
+
+    Map<String,String>phonebook = new TreeMap<String, String>();
+
+    private String disp;
+
+
+    //  Constructor
+
+    public PhoneBook(){
+    }
 
 
 
-    //Hey this is just a test to see if i can push shit
-    
+    //Methods
 
-     */
+    public void add (String name,String number){
+        phonebook.put(name,number);
+    }
+
+
+
+    public void remove (String name) {
+        phonebook.remove(name);
+    }
+
+
+    public String lookup(String name ){
+        return phonebook.get(name);
+    }
+
+
 }
--- src/main/java/phonebookpack/PhoneBook.java
+++ src/main/java/phonebookpack/PhoneBook.java
@@ -5,5 +5,34 @@ public class PhoneBook
     /*
     here is where Zach and Zohdi will compile all of the methods
     from the other "phonebook classes" after Troy, Lamar, and Javon submit them
-     */
+     */  //instance variable
+
+    Map<String,String>phonebook = new TreeMap<String, String>();
+
+    private String disp;
+
+
+    //  Constructor
+
+    public PhoneBook(){
+    }
+
+
+
+    //Methods
+
+    public void add (String name,String number){
+        phonebook.put(name,number);
+    }
+
+
+
+    public void remove (String name) {
+        phonebook.remove(name);
+    }
+
+
+    public String lookup(String name ){
+        return phonebook.get(name);
+    }
 }
